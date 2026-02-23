78e291a9f3b35da8c933ca3b2ca2a851f950683b
x224ch-web <x224ch@gmail.com>
2026-02-24 05:57:27 +0900

    Update login.js
    Signed-off-by: x224ch-web <x224ch@gmail.com>

--- login.js
+++ login.js
@@ -1,23 +1,26 @@
 export function render(container) {
 
   container.innerHTML = `
-    <h3>秘密のパスコードは、1234 です。</h3>
+    <div style="padding:20px;">
+      <h3>秘密のパスコードは、1234 です。</h3>
 
-    <input id="familyCode" placeholder="パスコード">
+      <input id="familyCode" placeholder="パスコード"><br><br>
 
-    <select id="userSelect">
-      <option value="">あなたは誰？</option>
-      <option>まよ</option>
-      <option>ほのか</option>
-      <option>りょう</option>
-      <option>しゅん</option>
-      <option>さとし</option>
-    </select>
+      <select id="userSelect">
+        <option value="">あなたは誰？</option>
+        <option>まよ</option>
+        <option>ほのか</option>
+        <option>りょう</option>
+        <option>しゅん</option>
+        <option>さとし</option>
+      </select><br><br>
 
-    <button id="loginBtn">入室</button>
+      <button id="loginBtn">入室</button>
+    </div>
   `;
 
   document.getElementById("loginBtn").onclick = () => {
+
     const code = document.getElementById("familyCode").value;
 
     if (code === "1234") {
@@ -25,5 +28,6 @@ export function render(container) {
     } else {
       alert("パスコード違います");
     }
+
   };
 }
