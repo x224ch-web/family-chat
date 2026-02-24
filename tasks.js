dd50056cbf5817513cbd48d6069736aac9b99a39
x224ch-web <x224ch@gmail.com>
2026-02-24 20:33:36 +0900

    Update tasks.js
    Signed-off-by: x224ch-web <x224ch@gmail.com>

--- tasks.js
+++ tasks.js
@@ -1,3 +1,28 @@
 export function render(container) {
-  container.innerHTML = "<h2>タスク</h2>";
+
+  container.innerHTML = `
+    <div style="padding:16px;">
+      <h2>タスク</h2>
+
+      <input id="title" placeholder="やること"><br><br>
+
+      <select id="assignee">
+        <option value="">担当者</option>
+        <option>まよ</option>
+        <option>ほのか</option>
+        <option>りょう</option>
+        <option>しゅん</option>
+        <option>さとし</option>
+      </select><br><br>
+
+      <input id="due" type="date"><br><br>
+
+      <button id="add">追加</button>
+
+      <hr>
+
+      <div id="list"></div>
+    </div>
+  `;
+
 }
