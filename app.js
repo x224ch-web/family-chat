86f1a2e4c9eb6b19d1488a6db5f11ee27e68be0a
x224ch-web <x224ch@gmail.com>
2026-02-25 20:18:36 +0900

    Update app.js
    Signed-off-by: x224ch-web <x224ch@gmail.com>

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import { render as loginRender } from "./login.js";
 
+// 🔹 Firebase 設定
+const firebaseConfig = {
+  apiKey: "AIzaSyC8qlv0imeiH6Vw_qu8295D_wno1xvEpKc",
+  authDomain: "x224ch-c937f.firebaseapp.com",
+  databaseURL: "https://x224ch-c937f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "x224ch-c937f",
+  storageBucket: "x224ch-c937f.appspot.com",
+  messagingSenderId: "217831065440",
+  appId: "1:217831065440:web:c15038f05ede6973448005"
+};
+
+// ⭐ Firebase 初期化
+initializeApp(firebaseConfig);
+
+// ⭐ アプリ開始
 const app = document.getElementById("app");
-
-const savedUser = localStorage.getItem("familyUser");
-
-if (savedUser) {
-  // ⭐ 自動ログイン
-  import("./chat.js").then(mod => {
-    mod.render(app);
-  });
-
-} else {
-  loginRender(app);
-}
+loginRender(app);
