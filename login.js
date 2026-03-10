92cdbe5c26b2108d3e08f8068e26e619ea6abf5e
x224ch-web <x224ch@gmail.com>
2026-03-11 00:13:19 +0900

    Update login.js
    Signed-off-by: x224ch-web <x224ch@gmail.com>

--- login.js
+++ login.js
@@ -1,67 +1,13 @@
-document.addEventListener("DOMContentLoaded",()=>{
-
-const cards=document.querySelectorAll(".card");
-
-const db=firebase.database();
-
-cards.forEach(card=>{
-
-const user=card.dataset.name;
-
-const badge=card.querySelector(".unread");
-
-/* unread */
-
-db.ref("messages").on("value",snap=>{
-
-let count=0;
-
-snap.forEach(m=>{
-
-if(m.val().user!==user) count++;
-
-});
-
-badge.style.display=count?"block":"none";
-
-badge.innerText=count;
-
-});
-
-/* login */
+document.querySelectorAll(".card").forEach(card=>{
 
 card.onclick=()=>{
+
+const user=card.dataset.user;
 
 localStorage.setItem("currentUser",user);
 
 location.reload();
 
 };
-
-});
-
-/* memo */
-
-document.querySelectorAll(".card-memo").forEach(memo=>{
-
-const user=memo.closest(".card").dataset.name;
-
-const saved=localStorage.getItem("memo_"+user);
-
-if(saved){
-
-memo.innerText=saved;
-
-memo.classList.remove("empty");
-
-}
-
-memo.oninput=()=>{
-
-localStorage.setItem("memo_"+user,memo.innerText);
-
-};
-
-});
 
 });
