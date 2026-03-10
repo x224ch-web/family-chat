868a61383880a84842dd15af2f75f01112e21eb9
x224ch-web <x224ch@gmail.com>
2026-03-11 03:36:44 +0900

    Update login.js
    Signed-off-by: x224ch-web <x224ch@gmail.com>

--- login.js
+++ login.js
@@ -11,3 +11,63 @@ location.reload();
 };
 
 });
+
+
+/* ===============================
+   unread badge
+=============================== */
+
+const users=["mayo","honoka","ryo","shun","satoshi"];
+
+const db=firebase.database().ref("messages");
+
+db.on("value",snap=>{
+
+const data=snap.val();
+
+if(!data)return;
+
+let unread={};
+
+users.forEach(u=>unread[u]=0);
+
+Object.values(data).forEach(msg=>{
+
+users.forEach(u=>{
+
+if(msg.user!==u){
+
+if(!msg.reads || !msg.reads[u]){
+
+unread[u]++;
+
+}
+
+}
+
+});
+
+});
+
+
+users.forEach(u=>{
+
+const badge=document.getElementById("badge-"+u);
+
+if(!badge)return;
+
+if(unread[u]>0){
+
+badge.style.display="block";
+
+badge.innerText=unread[u];
+
+}else{
+
+badge.style.display="none";
+
+}
+
+});
+
+});
