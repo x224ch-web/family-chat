0aa886473822c5cb210d348405862c883cb8e37f
x224ch-web <x224ch@gmail.com>
2026-03-11 00:26:30 +0900

    Update service-worker.js
    Signed-off-by: x224ch-web <x224ch@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,80 +1,18 @@
-const CACHE_NAME = "family-chat-v3";
+self.addEventListener("install",e=>{
 
-const urlsToCache = [
+e.waitUntil(
 
-"./",
-"./index.html",
-"./style.css",
-"./login.js",
-"./chat.js",
-"./icon-192.png",
-"./icon-512.png"
+caches.open("familychat").then(cache=>{
 
-];
+return cache.addAll([
 
-/* ===============================
-INSTALL
-=============================== */
+"/",
+"/index.html",
+"/style.css",
+"/login.js",
+"/chat.js"
 
-self.addEventListener("install", event => {
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-
-);
-
-self.skipWaiting();
-
-});
-
-
-/* ===============================
-ACTIVATE
-=============================== */
-
-self.addEventListener("activate", event => {
-
-event.waitUntil(
-
-caches.keys().then(cacheNames => {
-
-return Promise.all(
-
-cacheNames.map(cache => {
-
-if(cache !== CACHE_NAME){
-
-return caches.delete(cache);
-
-}
-
-})
-
-);
-
-})
-
-);
-
-self.clients.claim();
-
-});
-
-
-/* ===============================
-FETCH
-=============================== */
-
-self.addEventListener("fetch", event => {
-
-event.respondWith(
-
-caches.match(event.request)
-.then(response => {
-
-return response || fetch(event.request);
+]);
 
 })
 
